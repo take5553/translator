21cc181df0c352e0396511f30f2ba8f852def0ce
take555 <yamazakitakeshi02@gmail.com>
2018-12-01 22:53:14 +0900

    [add] add OGP image generator
    MiniMagickというgemファイルを追加してからOGP画像生成機能を入れました。
    動作にはImageMagickの導入が必要です。
    動作確認はlocalhost:3000/translator/result?text=（任意の文字列）
    まだパラメーターは一つしか受け入れられません。（要調整）

--- app/controllers/translator_controller.rb
+++ app/controllers/translator_controller.rb
@@ -1,4 +1,13 @@
 class TranslatorController < ApplicationController
   def index
   end
+
+  def result
+  	text = ogp_params[:text]
+    image = OgpCreator.build(text)
+  end
+
+  def ogp_params
+    params.permit(:text)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get 'translator/index'
+  get 'translator/result'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
